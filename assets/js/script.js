c13b42affbb6b91a01e028ca12367e89d463a20b
Marcus0224 <marcstlouis04@gmail.com>
2023-01-23 22:22:12 -0500

    capyure form values

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,21 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) { 
 
   event.preventDefault();
+
+  var taskNameInput = document.querySelector("input[name='task-name]").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
   
   var listItemEl = document.createElement("li"); 
   listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
+
+  // create div
+  var taskInfoEl = document.createElement("div");
+  // give it a class
+  taskInfoEl.className = "task-info";
+  //add HTML
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "<h3><span class='task-type'>" + taskTypeInput + "</span>";
+  
+  listItemEl.textContent = taskNameInput; 
   tasksToDoEl.appendChild(listItemEl); 
   }; 
 
